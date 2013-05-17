8335da0e2e07d82b50dee531da460c3b0d330517
rafaelgp <mr.rafaelgp@gmail.com>
2013-05-17 16:26:29 +0100

    add_structure() -> Add warning if source scene.sh didn't succeded. Echo attention and create scene.sh file
    if it doesn't exists

--- newvt.sh
+++ newvt.sh
@@ -223,11 +223,15 @@ EOF
 
     # source scene names for .sh files in root dir
     scenes_file=./$scenes_dir'.sh'
+    if [ $? != 0 ]; then
+        echo_attention "Unable to source scene.sh file"
+    fi
     if [ -f $scenes_file ]; then
         source $scenes_file
         echo -e "    Sourced $scenes_file\n" >> $log_file
     else
-        echo_attention "Unable to source scene.sh file"
+        echo_attention "$scene_file file NOT FOUND. Created one "
+        > ./$scenes_file
     fi
 
     echo_green "FOLDER TREE:" "added to $scenes_dir tour"
